0eedb4833cb313021ff12ccb02c18625dfdfb9d3
xdream oldlu <xdream@gmail.com>
2023-03-12 01:48:16 +0800

    fix wrong import

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,14 +1,11 @@
-import { useCallback, useReducer } from "react"
-import { ACTION_TYPE, TCard } from "../types"
-import { init } from "../utils"
+import { TCard } from "../types"
 import { useAnnaState } from "../utils/annaContext"
-import { stateReducer } from "../utils/reducer"
 import CardAdd from "./CardAdd"
 import CardItem from "./CardItem"
 import Editor from "./Editor"
 
 const CardList = () => {
-  const { state, dispatch } = useAnnaState()
+  const { state } = useAnnaState()
 
   return (
     <div className='flex flex-wrap items-start justify-center'>
