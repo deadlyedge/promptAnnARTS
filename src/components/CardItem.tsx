a4654f458c21985de5307f690a35b0afdb898bf3
xdream oldlu <xdream@gmail.com>
2023-03-12 01:53:38 +0800

    change brackets type to make code clean.

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -9,9 +9,9 @@ const CardItem = ({ card }: { card: TCard }) => {
     <div className='bg-cover bg-center w-96 m-1 bg-gray-900 rounded shadow static'>
       <CardHeader card={card} />
       <div className='text-xs relative'>
-        {bodyZones.map((zone, index) => {
-          return <CardBody card={card} zone={zone} key={index} />
-        })}
+        {bodyZones.map((zone, index) => (
+          <CardBody card={card} zone={zone} key={index} />
+        ))}
       </div>
     </div>
   )
