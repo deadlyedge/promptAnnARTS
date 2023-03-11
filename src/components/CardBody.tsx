57b0225aba6c8cad3da86691785367cca34575e9
xdream oldlu <xdream@gmail.com>
2023-03-11 21:56:03 +0800

    prepare to port to context...

--- src/components/CardBody.tsx
+++ src/components/CardBody.tsx
@@ -1,10 +1,13 @@
 import React, { FC, ReactElement, useCallback } from "react"
-import { TCard } from "../types"
+import { TCard, TPrompt } from "../types"
 
 interface ICardItem {
   card: TCard
   toggleTags: (cardID: string, id: string) => void
   refreshEditor: () => void
+}
+export interface IIndexable {
+  [key: string]: any
 }
 
 const CardBody: FC<ICardItem> = ({
@@ -17,8 +20,55 @@ const CardBody: FC<ICardItem> = ({
     refreshEditor()
   }, [])
 
+  const handleMulti = useCallback((zone: boolean) => {
+    zone
+      ? card.imageInfo.prompts.map((prompt) => toggleTags(card.id, prompt.id))
+      : card.imageInfo.negatives.map((prompt) => toggleTags(card.id, prompt.id))
+    refreshEditor()
+  }, [])
+
+  // const promptZone = () => {
+  //   const zoneList: IIndexable = ["prompts", "negatives"]
+  //   return zoneList.map((zone: string) => (
+  //     <div
+  //       className={
+  //         zone == "prompts"
+  //           ? "group/item bg-green-400 bg-opacity-40 block"
+  //           : "group/item bg-red-400 bg-opacity-40 block"
+  //       }>
+  //       {(card.imageInfo as IIndexable)[zone].map((prompt: TPrompt) => (
+  //         <button className='' key={prompt.id}>
+  //           <input
+  //             type='checkbox'
+  //             id={prompt.id}
+  //             className='hidden peer'
+  //             checked={prompt.checked}
+  //             onChange={() => handleSingle(prompt.id)}
+  //           />
+  //           <label
+  //             className='rounded-full m-0.5 px-1 text-xs text-gray-900 border-2 border-y-0 border-gray-100 bg-gray-200 bg-opacity-50 hover:bg-orange-600 hover:text-gray-50 duration-200 peer-checked:border-orange-600 peer-checked:bg-opacity-90'
+  //             htmlFor={prompt.id}>
+  //             {prompt.prompt}
+  //           </label>
+  //         </button>
+  //       ))}
+  //       <button
+  //         className='group invisible hover:bg-slate-200 group-hover/item:visible w-4 h-4 duration-100 hover:scale-125 rounded-full bg-lime-300 absolute right-1'
+  //         onClick={() => handleMulti(zone == "prompts")}>
+  //         <svg
+  //           className='w-4 h-4 mx-auto rotate-45 text-blue-500 group-hover:text-lime-500'
+  //           fill='currentColor'
+  //           viewBox='7 2 10 20'
+  //           xmlns='http://www.w3.org/2000/svg'>
+  //           <path d='M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z'></path>
+  //         </svg>
+  //       </button>
+  //     </div>
+  //   ))
+  // }
+
   return (
-    <div className='text-xs'>
+    <div className='text-xs relative'>
       <div className='group/item bg-green-400 bg-opacity-40 block'>
         {card.imageInfo.prompts.map((prompt) => (
           <button className='' key={prompt.id}>
@@ -36,7 +86,9 @@ const CardBody: FC<ICardItem> = ({
             </label>
           </button>
         ))}
-        <button className='group invisible hover:bg-slate-200 group-hover/item:visible w-4 h-4 duration-100 hover:scale-125 rounded-full bg-lime-300 absolute right-1'>
+        <button
+          className='group invisible hover:bg-slate-200 group-hover/item:visible w-4 h-4 duration-100 hover:scale-125 rounded-full bg-lime-300 absolute right-1'
+          onClick={() => handleMulti(true)}>
           <svg
             className='w-4 h-4 mx-auto rotate-45 text-blue-500 group-hover:text-lime-500'
             fill='currentColor'
@@ -63,7 +115,7 @@ const CardBody: FC<ICardItem> = ({
             </label>
           </button>
         ))}
-        <button className='group invisible hover:bg-slate-200 group-hover/item:visible w-4 h-4 duration-100 hover:scale-125 rounded-full bg-lime-300 absolute right-1'>
+        <button className='group invisible hover:bg-slate-200 group-hover/item:visible w-4 h-4 duration-100 hover:scale-125 rounded-full bg-lime-300 absolute right-1' onClick={() => handleMulti(false)}>
           <svg
             className='w-4 h-4 mx-auto rotate-45 text-blue-500 group-hover:text-lime-500'
             fill='currentColor'
@@ -82,7 +134,7 @@ const CardBody: FC<ICardItem> = ({
         <kbd className='rounded border border-gray-400 border-b-2 px-0.5'>
           +
         </kbd>{" "}
-        add ALL in that zone.
+        to reverse select in that zone.
       </label>
     </div>
   )
---
+++ src/utils/annaContext.tsx
@@ -0,0 +1,114 @@
+import {
+  createContext,
+  useContext,
+  useReducer,
+  Dispatch,
+  ReactNode,
+} from "react"
+import { ACTION_TYPE, IAction, IState, TCard } from "../types"
+
+// const initState = {
+//   editor: { prompts: "", negatives: "" },
+//   cardList: [],
+// }
+function initAnna(): IState {
+  return { editor: { prompts: "", negatives: "" }, cardList: [] }
+}
+
+const AnnaContext = createContext<{
+  state: IState
+  dispatch: Dispatch<IAction>
+}>({ state: initAnna(), dispatch: () => null })
+
+// const AnnaDispatchContext = createContext<IAction>({ dispatch: () => null })
+
+export function AnnaProvider({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(annaReducer, [], initAnna)
+
+  return (
+    <AnnaContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AnnaContext.Provider>
+  )
+}
+
+export function useAnnaState() {
+  return useContext(AnnaContext)
+}
+
+// export function useAnnaDispatch() {
+//   return useContext(AnnaDispatchContext)
+// }
+
+function annaReducer(state: IState, action: IAction): IState {
+  const { type, payload } = action
+
+  switch (type) {
+    // case ACTION_TYPE.INIT_CARD:
+    //   return {
+    //     ...state,
+    //     cardList: []
+    //   }
+    case ACTION_TYPE.ADD_CARD:
+      return {
+        ...state,
+        cardList: [...state.cardList, payload as TCard],
+      }
+    case ACTION_TYPE.REMOVE_CARD:
+      return {
+        ...state,
+        cardList: state.cardList.filter(
+          (item) => item.id !== (payload as string)
+        ),
+      }
+    case ACTION_TYPE.TOGGLE_TAGS:
+      return {
+        ...state,
+        cardList: state.cardList.map((card) =>
+          card.id === (payload as string[])[0]
+            ? {
+                ...card,
+                imageInfo: {
+                  ...card.imageInfo,
+                  prompts: card.imageInfo.prompts.map((tag) =>
+                    tag.id === (payload as string[])[1]
+                      ? { ...tag, checked: !tag.checked }
+                      : { ...tag }
+                  ),
+                  negatives: card.imageInfo.negatives.map((tag) =>
+                    tag.id === (payload as string[])[1]
+                      ? { ...tag, checked: !tag.checked }
+                      : { ...tag }
+                  ),
+                },
+              }
+            : { ...card }
+        ),
+      }
+    case ACTION_TYPE.REFRESH_EDITOR:
+      return {
+        ...state,
+        editor: {
+          prompts: state.cardList
+            .map((card) => card.imageInfo.prompts.filter((tag) => tag.checked))
+            .map((card) =>
+              card
+                .map((tags) => tags.prompt)
+                // .filter((i) => i && i.trim())
+                .join(", ")
+            )
+            .filter((i) => i && i.trim())
+            .join(", "),
+          negatives: state.cardList
+            .map((card) =>
+              card.imageInfo.negatives.filter((tag) => tag.checked)
+            )
+            .map((card) => card.map((tags) => tags.prompt).join(", "))
+            .filter((i) => i && i.trim())
+            .join(", "),
+        },
+      }
+    default:
+      return state
+  }
+}
